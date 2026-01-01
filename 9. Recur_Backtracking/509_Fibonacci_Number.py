ddd216af35bf2dce7e7b636a7e9382b7f15504eb
fushi <kaiki0201a@gmail.com>
2026-01-01 15:30:58 +0700

    34_RecurFibo_CoNho

--- 9. Recur_Backtracking/509_Fibonacci_Number.py
+++ 9. Recur_Backtracking/509_Fibonacci_Number.py
@@ -1,11 +1,23 @@
 #Fib: 0 1 1 2
 
 class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
+    #B1. Tao mot cai so tay:
+    def __init__(self):
+        self.memo = {}
 
+    def fib(self, n: int) -> int:
+        #B2. Kiem tra co trong memo chua
+        if n in self.memo:
+            return self.memo[n]
+        
+        #B3. Neu khong co thi de quy de tinh
         else:
-            return self.fib(n-1) + self.fib(n-2)
+            if n <= 1:      #Base case
+                return n
+
+            else:   #Vua de quy vua luu lai vao self.memo
+                res = self.fib(n-1) + self.fib(n-2)
+                self.memo[n] = res
+                return res
 p1 = Solution()
 print(p1.fib(2))
